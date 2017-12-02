7fb6066806219af381615abbccc936dcbae7e250
mcalvillo1212 <miguel@mcalvillo.com>
2017-12-02 01:33:38 -0500

    Added BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,29 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     gulpif = require('gulp-if'),
     // browserify = require('gulp-browserify'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
     concat = require('gulp-concat');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "builds/prod"
+    });
+    gulp.watch("components/sass/**/*.scss", ['sass']);
+    gulp.watch("builds/prod/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("components/sass/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("builds/prod/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('sync', ['serve']);
+
 
 var jsSources = [
   // 'components/scripts/vendor.js',
